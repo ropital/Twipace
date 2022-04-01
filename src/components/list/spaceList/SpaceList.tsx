cdc8c6039d6b415dc591864a83ee499356095d1c
Sho Sakai <ropitaru88@gmail.com>
2022-04-02 01:23:20 +0900

    update: Make it responsible

--- src/components/list/spaceList/SpaceList.tsx
+++ src/components/list/spaceList/SpaceList.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const SpaceList: VFC<Props> = ({ spaces }: Props) => {
   return (
-    <div className="w-1/3 gap-y-4 flex flex-col">
+    <div className="w-4/5 gap-y-4 flex flex-col md:w-1/3">
       {spaces.map((space) => {
         return <SpaceListItemLink key={space.id} {...space} />;
       })}
